c69303c50bce097cb3875e954593daec82bc9144
KosukeNakagawa95 <kna0891@gmail.com>
2026-03-19 22:09:24 +0900

    プロジェクトファイルを追加。

---
+++ AINewsFetcher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        var url = "https://news.google.com/rss/search?q=AI&hl=en-US&gl=US&ceid=US:en";
+
+        using var client = new HttpClient();
+        var rss = await client.GetStringAsync(url);
+
+        Console.WriteLine(rss);
+
+    }
+}
